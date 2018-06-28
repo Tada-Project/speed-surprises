d2e29db89f6d21b60d76d570e1dcd3245f0079e3
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-06-28 18:06:50 -0400

    Moving around the test_copies tests into a more logical ordering.

--- tests/test_copies.py
+++ tests/test_copies.py
@@ -8,6 +8,13 @@ from hypothesis import Verbosity
 from hypothesis.strategies import integers
 from speedsurprises.text import copies
 
+@pytest.mark.benchmark
+def test_count_benchmark(benchmark):
+    """Benchmark the mcopies_ofc function"""
+    copied_character_string = benchmark(copies.mcopies_ofc, input_string="100")
+    assert len(copied_character_string) == 100
+    assert copied_character_string.count("C") == 100
+
 
 @given(copies_as_int=integers(min_value=1))
 @settings(verbosity=Verbosity.verbose, deadline=None)
@@ -21,21 +28,6 @@ def test_letter_count_hypothesis(copies_as_int):
     assert copied_character_string.count("C") == copy_count
 
 
-@pytest.mark.benchmark
-def test_count_benchmark(benchmark):
-    """Benchmark the mcopies_ofc function"""
-    copied_character_string = benchmark(copies.mcopies_ofc, input_string="100")
-    assert len(copied_character_string) == 100
-    assert copied_character_string.count("C") == 100
-
-
-def test_letter_count_single():
-    """Returns output with correct number of copies"""
-    copied_character_string = copies.mcopies_ofc("10")
-    assert len(copied_character_string) == 10
-    assert copied_character_string.count("C") == 10
-
-
 @pytest.mark.parametrize("letter_count,expected_count", [
     ("1", 1),
     ("10", 10),
@@ -47,3 +39,10 @@ def test_letter_count_multiple(letter_count, expected_count):
     copied_character_string = copies.mcopies_ofc(letter_count)
     assert len(copied_character_string) == expected_count
     assert copied_character_string.count("C") == expected_count
+
+
+def test_letter_count_single():
+    """Returns output with correct number of copies"""
+    copied_character_string = copies.mcopies_ofc("10")
+    assert len(copied_character_string) == 10
+    assert copied_character_string.count("C") == 10
