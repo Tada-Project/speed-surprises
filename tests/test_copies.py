718f27a2b4abbba4ba7e22d9cc7bb80878196b51
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-06-28 16:53:19 -0400

    Adding a parameterized test case to the test_copies file.

--- tests/test_copies.py
+++ tests/test_copies.py
@@ -1,10 +1,24 @@
 """Tests for the gatorgrader module"""
 
+import pytest
+
 from speedsurprises.text import copies
 
 
-def test_correct_true_readable():
+def test_letter_count_single():
     """Returns output with correct number of copies"""
     copied_character_string = copies.mcopies_ofc("10")
     assert len(copied_character_string) == 10
     assert copied_character_string.count("C") == 10
+
+
+@pytest.mark.parametrize("letter_count,expected_count", [
+    ("10", 10),
+    ("100", 100),
+    ("1000", 1000),
+])
+def test_letter_count_multiple(letter_count, expected_count):
+    """Check that it can detect zero or one paragraphs"""
+    copied_character_string = copies.mcopies_ofc(letter_count)
+    assert len(copied_character_string) == expected_count
+    assert copied_character_string.count("C") == expected_count
