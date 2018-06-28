8837d8747f00b00cc4a7eb0a5160c898fa200b7f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-06-28 14:01:13 -0400

    Adding a conftest.py file for the test suites; pytest working.

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+"""Configuration file for the test suite"""
+
+import os
+import sys
+
+# set the system path to contain the previous directory
+mypath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, mypath + "/../")
