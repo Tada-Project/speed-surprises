75ef90d20ed836a9e81209ffef80519f6a2e245e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-06-28 18:41:41 -0400

    Improving the comments in the copies file.

--- speedsurprises/text/copies.py
+++ speedsurprises/text/copies.py
@@ -10,7 +10,7 @@ def mcopies_ofc(input_string):
     """Create copy_count (number in input_string) copies of the letter 'C'"""
     copy_count = int(input_string)
     list_of_letter_c = []
-    # iterate copy_count times, appending a ``C'' character at each iteration
+    # iterate copy_count times, appending a 'C' character at each iteration
     for i in range(copy_count):
         list_of_letter_c.append(LETTER_C)
     # join all the C's together into a single string, with no separator
@@ -21,8 +21,8 @@ def mcopies_of(input_string, character):
     """Create copy_count (number in input_string) copies of the character"""
     copy_count = int(input_string)
     list_of_letter = []
-    # iterate copy_count times, appending a ``C'' character at each iteration
+    # iterate copy_count times, appending a character at each iteration
     for i in range(copy_count):
         list_of_letter.append(character)
-    # join all the C's together into a single string, with no separator
+    # join all the letters together into a single string, with no separator
     return "".join(list_of_letter)
