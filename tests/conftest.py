c82249f04ada3d93c570c17e556347a1e3406ee4
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-06-28 17:25:45 -0400

    Adding comments to the conftest and then adding an option for pytest.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,16 @@ import os
 import sys
 
 # set the system path to contain the previous directory
-mypath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, mypath + "/../")
+# because it is the root directory that contains the
+# directory for the package, sub-packages, and modules
+MYPATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, MYPATH + "/../")
+
+
+# define the viable command-line arguments when running tests
+def pytest_addoption(parser):
+    """Add options for running certain marked pytest tests"""
+    parser.addoption(
+        "--runbenchmark",
+        action="store_true",
+        help="Run the benchmark test cases")
