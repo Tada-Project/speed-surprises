121e6a3821dfcb661c4e2cd8706f20d5abdd25b8
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-06-28 15:10:56 -0400

    Fixing the comments and then adding assertions to the test_copies.

--- tests/test_copies.py
+++ tests/test_copies.py
@@ -4,6 +4,7 @@ from speedsurprises.text import copies
 
 
 def test_correct_true_readable():
-    """Ensures that the returned String is of correct length"""
+    """Returns output with correct number of copies"""
     copied_character_string = copies.mcopies_ofc("10")
-    print(copied_character_string)
+    assert len(copied_character_string) == 10
+    assert copied_character_string.count("C") == 10
