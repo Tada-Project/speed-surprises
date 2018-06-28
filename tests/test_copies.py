40a0c63260f6734abb4d147a5dd69dc5e995f22f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-06-28 17:02:50 -0400

    Adding a pytest-benchmark to the test_copies file.

--- tests/test_copies.py
+++ tests/test_copies.py
@@ -5,6 +5,13 @@ import pytest
 from speedsurprises.text import copies
 
 
+def test_count_benchmark(benchmark):
+    """Benchmark the mcopies_ofc function"""
+    copied_character_string = benchmark(copies.mcopies_ofc, input_string="10")
+    assert len(copied_character_string) == 10
+    assert copied_character_string.count("C") == 10
+
+
 def test_letter_count_single():
     """Returns output with correct number of copies"""
     copied_character_string = copies.mcopies_ofc("10")
@@ -19,7 +26,7 @@ def test_letter_count_single():
     ("1000", 1000),
 ])
 def test_letter_count_multiple(letter_count, expected_count):
-    """Check that it can detect zero or one paragraphs"""
+    """Returns output with correct number of copies"""
     copied_character_string = copies.mcopies_ofc(letter_count)
     assert len(copied_character_string) == expected_count
     assert copied_character_string.count("C") == expected_count
