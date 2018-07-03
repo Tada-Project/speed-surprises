4e000ecad8eb40b438e596ddbb70ec70dec42e47
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-03 15:22:05 -0400

    Removing not-needed imports and making perf_copies_ofc a little more general.

--- perf_mcopies_ofc.py
+++ perf_mcopies_ofc.py
@@ -1,12 +1,12 @@
 """Benchmarks with perf for the functions in the copies module"""
 
-import sys
-
 import perf
 
 from speedsurprises.text import copies
 
 CONFIGURATION = ".configuration.txt"
+DESCRIPTION_METANAME = "description"
+PERF_EXPERIMENT_NAME = "perf_mcopies_ofc"
 
 
 def bench_copy_function(copy_function, current_chosen_size):
@@ -14,7 +14,7 @@ def bench_copy_function(copy_function, current_chosen_size):
     copy_function(current_chosen_size)
 
 
-# Example of calling the function:
+# Example of calling the function under analysis:
 # copied_character_string = copies.mcopies_ofc(copies_as_string)
 
 if __name__ == "__main__":
@@ -25,11 +25,11 @@ if __name__ == "__main__":
     # configure perf
     runner = perf.Runner()
     # configure the run of the benchmark
-    experiment_name = "mcopies_ofc" + str(chosen_size)
-    runner.metadata["description"] = experiment_name
+    current_experiment_name = PERF_EXPERIMENT_NAME + str(chosen_size)
+    runner.metadata[DESCRIPTION_METANAME] = current_experiment_name
     # run the perf benchmark for the function
     benchmark = runner.bench_func(
         "mcopies", bench_copy_function, copies.mcopies_ofc, chosen_size
     )
     # save the perf results from running the benchmark
-    benchmark.dump("results/" + experiment_name + ".json", compact=False, replace=True)
+    benchmark.dump("results/" + current_experiment_name + ".json", compact=False, replace=True)
