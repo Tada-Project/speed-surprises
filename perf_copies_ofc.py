78a91fe7b0427cc3d2ac0d60ddb1801a613f4a01
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-03 11:48:54 -0400

    Starting to delete code from the perf_copies_ofc.

--- perf_copies_ofc.py
+++ perf_copies_ofc.py
@@ -1,12 +1,14 @@
 """Benchmarks with perf for the functions in the copies module"""
 
+import sys
+
 import perf
 
 from speedsurprises.text import copies
 
 
 def bench_copy_function(copy_function, chosen_size):
-    """Run a benchmark"""
+    """Run a copy benchmark for a copy_function and a chosen_size"""
     copy_function(chosen_size)
 
 
@@ -14,27 +16,21 @@ def bench_copy_function(copy_function, chosen_size):
 # copied_character_string = copies.mcopies_ofc(copies_as_string)
 
 # Idea: imagine calling this iteratively multiple times with larger
-# inputs (size by doubling experiment and data by hypothesis)
+# inputs (chosen_size by doubling experiment and data by hypothesis)
 
 
 if __name__ == "__main__":
+    # Read the command-line argument chosen_size
+    chosen_size = sys.argv[1:]
     # configure perf
     runner = perf.Runner()
-    # setup parameters of simple doubling experiment
-    size = 100
-    factor = 2
-    size_stop = 1600
     # perform the small doubling experiment
-    while size <= size_stop:
-        print("Start running experiment for size " + str(size) + "...")
-        experiment_name = "mcopies_ofc" + str(size)
-        runner.metadata["description"] = experiment_name
-        benchmark = runner.bench_func(
-            "mcopies", bench_copy_function, copies.mcopies_ofc, size
-        )
-        time.sleep(2)
-        benchmark.dump(
-            "results/" + experiment_name + ".json", compact=False, replace=True
-        )
-        size = size * 2
-        print("... Done running experiment for size " + str(size))
+    print("Start running experiment for chosen_size " + str(chosen_size) + "...")
+    experiment_name = "mcopies_ofc" + str(chosen_size)
+    runner.metadata["description"] = experiment_name
+    benchmark = runner.bench_func(
+        "mcopies", bench_copy_function, copies.mcopies_ofc, chosen_size
+    )
+    benchmark.dump("results/" + experiment_name + ".json", compact=False, replace=True)
+    chosen_size = chosen_size * 2
+    print("... Done running experiment for chosen_size " + str(chosen_size))
