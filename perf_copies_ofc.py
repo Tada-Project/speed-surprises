22664a6aeb77333745437ade5e25cb405dfda50c
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-03 12:06:20 -0400

    Adding more descriptive comments to the perf_copies_ofc.

--- perf_copies_ofc.py
+++ perf_copies_ofc.py
@@ -16,16 +16,18 @@ def bench_copy_function(copy_function, chosen_size):
 # copied_character_string = copies.mcopies_ofc(copies_as_string)
 
 if __name__ == "__main__":
-    # Read the chosen_size
+    # read the chosen_size
     filepath = "configuration.txt"
     with open(filepath) as fp:
         chosen_size = fp.readline().replace("\n", "")
     # configure perf
     runner = perf.Runner()
-    # run the function
+    # configure the run of the benchmark
     experiment_name = "mcopies_ofc" + str(chosen_size)
     runner.metadata["description"] = experiment_name
+    # run the perf benchmark for the function
     benchmark = runner.bench_func(
         "mcopies", bench_copy_function, copies.mcopies_ofc, chosen_size
     )
+    # save the perf results from running the benchmark
     benchmark.dump("results/" + experiment_name + ".json", compact=False, replace=True)
