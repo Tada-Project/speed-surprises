db2dd8618fb4bab81cd43ff2d6c0fd3ce721f0b1
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-06 16:36:39 -0400

    Adding the start to the test_factorial for a Hypothesis-driven test.

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -2,10 +2,10 @@
 
 import pytest
 
-# from hypothesis import given
-# from hypothesis import settings
-# from hypothesis import Verbosity
-# from hypothesis.strategies import integers
+from hypothesis import given
+from hypothesis import settings
+from hypothesis import Verbosity
+from hypothesis.strategies import integers
 from speedsurprises.numbers import factorial
 
 
@@ -14,3 +14,14 @@ def test_count_benchmark(benchmark):
     """Benchmark the compute_factorial function"""
     computed_value = benchmark(factorial.compute_factorial, value=10)
     assert computed_value == 3628800
+
+
+@given(factorial_input=integers(min_value=1, max_value=10))
+@settings(verbosity=Verbosity.verbose, deadline=None)
+@pytest.mark.hypothesisworks
+def test_letter_count_hypothesis(factorial_input):
+    """Returns output with correct factorial number"""
+    computed_value = factorial.compute_factorial(factorial_input)
+    previous_computed_value = factorial.compute_factorial(factorial_input - 1)
+    assert computed_value > 0
+    assert previous_computed_value > 0
