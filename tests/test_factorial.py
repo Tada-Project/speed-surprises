c9c1b70975f26c0bf28bcac1efc41e9b27298c98
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-06 17:33:16 -0400

    Adding a parameterized test case to the test_factorial suite.

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -26,3 +26,13 @@ def test_factorial_hypothesis(factorial_input):
     assert computed_value > 0
     assert previous_computed_value > 0
     assert computed_value == factorial_input * previous_computed_value
+
+
+@pytest.mark.parametrize(
+    "factorial_input,expected_answer",
+    [(1, 1), (2, 2 * 1), (3, 3 * 2 * 1), (4, 4 * 3 * 2 * 1)],
+)
+def test_factorial_multple(factorial_input, expected_answer):
+    """Check the compute_factorial function with multiple inputs"""
+    computed_value = factorial.compute_factorial(factorial_input)
+    assert computed_value == expected_answer
