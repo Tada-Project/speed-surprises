d7676a7017080a64e11e0f14b89b8aedf56e4f01
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-13 12:27:46 -0400

    Adding a more extensive test case to test_issubset handling empty sets.

--- tests/test_issubset.py
+++ tests/test_issubset.py
@@ -25,10 +25,29 @@ def test_issubset_benchmark(benchmark):
 )
 @settings(verbosity=Verbosity.verbose, deadline=None)
 @pytest.mark.hypothesisworks
-def test_issubset_hypothesis_integer_lists(first_list, second_list):
+def test_issubset_hypothesis_integer_lists_yes(first_list, second_list):
     """Returns output with correct factorial number"""
     fully_containing_list = [1, 2, 3, 4, 5]
     determined_is_subset_first = sets.is_subset(first_list, fully_containing_list)
     determined_is_subset_second = sets.is_subset(second_list, fully_containing_list)
     assert determined_is_subset_first is True
     assert determined_is_subset_second is True
+
+
+@given(
+    first_list=lists(elements=integers(min_value=1, max_value=5), min_size=0),
+    second_list=lists(elements=integers(min_value=1, max_value=5), min_size=1),
+)
+@settings(verbosity=Verbosity.verbose, deadline=None)
+@pytest.mark.hypothesisworks
+def test_issubset_hypothesis_integer_lists_no(first_list, second_list):
+    """Returns output with correct factorial number"""
+    not_fully_containing_list = [11, 12, 13, 14, 15]
+    determined_is_subset_first = sets.is_subset(first_list, not_fully_containing_list)
+    determined_is_subset_second = sets.is_subset(second_list, not_fully_containing_list)
+    # Note that the empty set is a subset of all possible sets
+    if not first_list:
+        assert determined_is_subset_first is True
+    else:
+        assert determined_is_subset_first is False
+    assert determined_is_subset_second is False
