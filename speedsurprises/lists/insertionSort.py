56534c6a53c93942d93241b0d9c53212c2810be6
Spencer Huang <31478964+huangs1@users.noreply.github.com>
2018-10-29 16:04:15 -0400

    Edited for checkstyle

--- speedsurprises/lists/insertionSort.py
+++ speedsurprises/lists/insertionSort.py
@@ -6,13 +6,10 @@
 # Worst-case time complexity: O(n^2)
 
 def insertionSort(list):
-   for i in range(1, len(list)):
-
-     currentValue = list[i]
-     position = i
-
-     while position > 0 and list[position - 1] > currentValue:
-         list[position] = list[position - 1]
-         position -= 1
-
-     list[position] = currentValue
+    for i in range(1, len(list)):
+        currentValue = list[i]
+        position = i
+        while position > 0 and list[position - 1] > currentValue:
+            list[position] = list[position - 1]
+            position -= 1
+        list[position] = currentValue
