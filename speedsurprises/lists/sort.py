a9e25754630bcd51e18910939ee721a90231579a
clussier <lussierc@allegheny.edu>
2018-10-28 21:45:58 -0400

    Adding code for a BubbleSort function.

---
+++ speedsurprises/lists/sort.py
@@ -0,0 +1,9 @@
+"""Do list sorting using BubbleSort"""
+
+def bubbleSort(list):
+    for passnum in range(len(list)-1,0,-1):
+        for i in range(passnum):
+            if list[i]>list[i+1]:
+                temp = list[i]
+                list[i] = list[i+1]
+                list[i+1] = temp
