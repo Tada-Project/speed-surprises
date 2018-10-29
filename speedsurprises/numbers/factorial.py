7517012fb5d8b69f39a656e7377cb11512aa364e
Lancaster Wu <31478979+Lancasterwu@users.noreply.github.com>
2018-10-29 16:29:20 -0400

    removed Fibonacci

--- speedsurprises/numbers/factorial.py
+++ speedsurprises/numbers/factorial.py
@@ -20,17 +20,3 @@ def compute_recursive_factorial(value):
         return 1
     else:
         return value * compute_recursive_factorial(value - 1)
-
-def compute_iterative_fibonacci(value):
-    if (value == 0):
-        return value
-    elif (value <= 2):
-        return 1
-    else:
-        x = 0
-        y = 1
-        for i in range(1, value):
-            z = (x + y)
-            x = y
-            y = z
-        return y
