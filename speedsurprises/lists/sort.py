f28a3cf3d156629f63c533a0bdb50b0be147c58a
clussier <lussierc@allegheny.edu>
2018-10-28 21:52:57 -0400

    Adding necessary comments/information regarding the bubble sort function.

--- speedsurprises/lists/sort.py
+++ speedsurprises/lists/sort.py
@@ -1,6 +1,12 @@
 """Do list sorting using BubbleSort"""
 
+# Source and/or inspiration for the function(s):
+# https://bit.ly/2pXGWai
+
+# Worst-case time complexity: O(n^2)
+
 def bubbleSort(list):
+    """Sorts a list using BubbleSort function."""
     for passnum in range(len(list)-1,0,-1):
         for i in range(passnum):
             if list[i]>list[i+1]:
