df4cfcfea329d6946b6908ca7202eee4d9529d00
clussier <lussierc@allegheny.edu>
2018-10-29 17:52:10 -0400

    Creating test suite for iterative fibonacci.

---
+++ speedsurprises/numbers/test_iterative_fibonacci.py
@@ -0,0 +1,48 @@
+"""Tests for the compute_iterative_fibonacci function in the recursivefibonacci module of the numbers package"""
+
+import pytest
+
+from hypothesis import given
+from hypothesis import settings
+from hypothesis import Verbosity
+from hypothesis.strategies import integers
+
+from speedsurprises.numbers import fibonacci
+
+
+@pytest.mark.benchmark
+def test_iterative_fibonacci_benchmark(benchmark):
+    """Benchmark the compute_iterative_fibonacci function"""
+    computed_value = benchmark(fibonacci.compute_iterative_fibonacci, value=19)
+    assert computed_value == 4181
+
+
+@given(fibonacci_input=integers(min_value=1, max_value=10))
+@settings(verbosity=Verbosity.verbose, deadline=None)
+@pytest.mark.hypothesisworks
+def test_fibonacci_hypothesis(fibonacci_input):
+    """Returns output with correct fibonacci value"""
+    computed_value = fibonacci.compute_iterative_fibonacci(fibonacci_input)
+    previous_computed_value = fibonacci.compute_iterative_fibonacci(fibonacci_input - 1)
+    goldenratio = 1.61803398875 # The golden ratio can be used to compute fibonacci values. Multiply the previous fibonacci value by the ratio to get the next value.
+    assert computed_value > 0
+    assert previous_computed_value > 0
+    assert computed_value == math.ceil(goldenratio * previous_computed_value) # used math.ceil to correct possible rounding errors
+
+
+@pytest.mark.parametrize(
+    "fibonacci_input,expected_answer",
+    [(1, 1), (2, 1), (3, 2), (4, 3) (5, 5)],
+)
+
+
+def test_fibonacci_multiple(fibonacci_input, expected_answer):
+    """Check the compute_iterative_fibonacci function with multiple inputs"""
+    computed_value = fibonacci.compute_iterative_fibonacci(fibonacci_input)
+    assert computed_value == expected_answer
+
+
+def test_fibonacci_single():
+    """Check the compute_iterative_fibonacci function with a single input"""
+    computed_value = fibonacci.compute_iterative_fibonacci(18)
+    assert computed_value == 2584
