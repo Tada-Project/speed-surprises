856f2735082c6f3a56f936c5e13f34dd2bdcf9c5
Spencer Huang <31478964+huangs1@users.noreply.github.com>
2018-10-29 16:05:16 -0400

    Edited for checkstyle

--- tests/test_insertionSort.py
+++ tests/test_insertionSort.py
@@ -8,19 +8,21 @@ from hypothesis import Verbosity
 from hypothesis.strategies import integers
 from hypothesis.strategies import lists
 
-from speedsurprises.lists import sorting
+from speedsurprises.lists import insertionSorting
+
 
 @pytest.mark.benchmark
 def test_insertionSort_benchmark(benchmark):
     """Benchmark the insertionSort function"""
-    sorted_list = benchmark(sorting.insertionSort, list = [4, 2, 3, 1])
+    sorted_list = benchmark(sorting.insertionSort, list=[4, 2, 3, 1])
     assert sorted_list == [1, 2, 3, 4]
 
-@given(list = lists(elements = integers(min_value = 1, max_value = 10), min_size = 2))
-@settings(verbosity = Verbosity.verbose, deadline = None)
+
+@given(list=lists(elements=integers(min_value=1, max_value=10), min_size=2))
+@settings(verbosity=Verbosity.verbose, deadline=None)
 @pytest.mark.hypothesisworks
 def test_insertionSort_hypothesis(list):
-    """Sees if list of hypothesis-generated data sorted using insertionSort is equal to same data sorted using Python's sort function"""
+    """Sees if sorted list from hypothesis-generated data is the same as Python's sort function"""
     insertionSort_list = sorting.insertionSort(list)
     python_sort_list = sort.list()
     assert inerstionSort_list == python_sort_list
@@ -40,3 +42,4 @@ def test_insertionSort_single():
     """Check the insertionSort function with a single list"""
     sorted_list = sorting.insertionSort([4, 3, 1, 5, 6])
     assert sorted_list == [1, 3, 4, 5, 6]
+
