b651a74c5feacbef8c9be890f37c13d8b04b06ee
clussier <lussierc@allegheny.edu>
2018-11-11 15:50:34 -0500

    Fixed issue for test cases where expected_answer could not be found.

--- tests/test_bubble_sort.py
+++ tests/test_bubble_sort.py
@@ -19,14 +19,16 @@ def test_bubble_sort_benchmark(benchmark):
 
 
 @given(
-    list=lists(elements=integers(min_value=1, max_value=10), min_size=2),
+    test_list=lists(elements=integers(min_value=1, max_value=10), min_size=2),
 )
 @settings(verbosity=Verbosity.verbose, deadline=None)
 @pytest.mark.hypothesisworks
-def test_bubble_sort_hypothesis_integer_lists_yes(list):
-    """Sees if list of hypothesis-generated data sorted using bubble_sort is equal to same data sorted using Python's sort function"""
-    bubble_sort_list = sorting.bubble_sort(list)
-    python_sort_list = list.sort()
+def test_bubble_sort_hypothesis_integer_lists_yes(test_list):
+    """Sees if list of hypothesis-generated data sorted using bubble_sort is
+    equal to same data sorted using Python's sort function"""
+    bubble_sort_list = sorting.bubble_sort(test_list)
+    # pylint: disable=W0622
+    python_sort_list = test_list.sort()
     assert bubble_sort_list == python_sort_list
 
 
@@ -34,7 +36,7 @@ def test_bubble_sort_hypothesis_integer_lists_yes(list):
     "test_list, expected_answer",
     [([5, 3, 9, 2, 1]), ([1, 2, 3, 5, 9])],
 )
-def test_issubset_multiple(test_list):
+def test_issubset_multiple(test_list, expected_answer):
     """Check the is_subset function with multiple inputs"""
     sorted_list = sorting.bubble_sort(test_list)
     assert sorted_list == expected_answer
