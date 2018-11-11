8886f08c79a7fabaa1643eade559fd10af9d60de
Lancaster Wu <31478979+Lancasterwu@users.noreply.github.com>
2018-11-11 16:02:53 -0500

    Update booleancopies.py

--- speedsurprises/boolean/booleancopies.py
+++ speedsurprises/boolean/booleancopies.py
@@ -3,12 +3,12 @@
 Boolean_T = True
 
 
-def bcopies_oft(booleanlist):
+def bcopies_oft(inputlength):
     """Create empty list for boolean true"""
     list_of_boolean_true = []
     # iterate length of booleanlist times, appending a boolean true at each iteration
     # pylint: disable=unused-variable
-    for i in range(len(booleanlist)):
+    for i in range(int(inputlength)):
         list_of_boolean_true.append(Boolean_T)
     # return copied list
     return list_of_boolean_true
@@ -19,7 +19,7 @@ def bcopies_of(booleanlist, boolean):
     list_of_boolean_true = []
     # iterate length of booleanlist times, appending a boolean true at each iteration
     # pylint: disable=unused-variable
-    for i in range(len(booleanlist)):
+    for i in range(int(inputlength)):
         list_of_boolean_true.append(boolean)
     # return copied list
     return list_of_boolean_true
