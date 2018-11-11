815a3cdcca3f8953a5105fb6993a7c2a50de5149
Lancaster Wu <31478979+Lancasterwu@users.noreply.github.com>
2018-11-11 16:26:34 -0500

    Update and rename test_b_copies_ofc.py to test_b_copies_oft.py

--- tests/test_b_copies_oft.py
+++ tests/test_b_copies_oft.py
@@ -8,7 +8,7 @@ from speedsurprises.boolean import booleancopies
 @pytest.mark.benchmark
 def test_count_benchmark(benchmark):
     """Benchmark the mcopies_ofc function"""
-    copied_boolean_list = benchmark(booleancopies.bcopies_ofc, inputlength="100")
+    copied_boolean_list = benchmark(booleancopies.bcopies_oft, inputlength="100")
     assert len(copied_boolean_list) == 100
     assert copied_boolean_list.count(True) == 100
 
@@ -18,13 +18,13 @@ def test_count_benchmark(benchmark):
 )
 def test_boolean_count_multiple(boolean_count, expected_count):
     """Returns output with correct number of copies"""
-    copied_boolean_list = booleancopies.bcopies_ofc(boolean_count)
+    copied_boolean_list = booleancopies.bcopies_oft(boolean_count)
     assert len(copied_boolean_list) == expected_count
     assert copied_boolean_list.count(True) == expected_count
 
 
 def test_boolean_count_single():
     """Returns output with correct number of copies"""
-    copied_boolean_list = booleancopies.bcopies_ofc("10")
+    copied_boolean_list = booleancopies.bcopies_oft("10")
     assert len(copied_boolean_list) == 10
     assert copied_boolean_list.count(True) == 10
