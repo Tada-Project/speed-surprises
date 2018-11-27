cb151e2b8ccf8c7e0659c11858f66b6620ae168e
Lancaster Wu <31478979+Lancasterwu@users.noreply.github.com>
2018-11-27 18:07:30 -0500

    Added the white spaces

--- speedsurprises/lists/sorting.py
+++ speedsurprises/lists/sorting.py
@@ -46,33 +46,33 @@ def bubble_sort(list):
 
 def mergeSort(list):
     """Sorts a list using MergeSort function."""
-    if len(list)>1:
-        mid = len(list)//2
+    if len(list) > 1:
+        mid = len(list) // 2
         lefthalf = list[:mid]
         righthalf = list[mid:]
 
         mergeSort(lefthalf)
         mergeSort(righthalf)
 
-        i=0
-        j=0
-        k=0
+        i = 0
+        j = 0
+        k = 0
         while i < len(lefthalf) and j < len(righthalf):
             if lefthalf[i] < righthalf[j]:
-                list[k]=lefthalf[i]
-                i=i+1
+                list[k] = lefthalf[i]
+                i = i + 1
             else:
-                list[k]=righthalf[j]
-                j=j+1
-            k=k+1
+                list[k] = righthalf[j]
+                j = j + 1
+            k = k + 1
 
         while i < len(lefthalf):
-            list[k]=lefthalf[i]
-            i=i+1
-            k=k+1
+            list[k] = lefthalf[i]
+            i = i + 1
+            k = k + 1
 
         while j < len(righthalf):
-            list[k]=righthalf[j]
-            j=j+1
-            k=k+1
+            list[k] = righthalf[j]
+            j = j + 1
+            k = k + 1
     return list
