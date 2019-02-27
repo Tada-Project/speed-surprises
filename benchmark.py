fdcb27b58ac1c6c1a56c87c4dff3e8b82b0c7cb8
clussier <lussierc@allegheny.edu>
2019-02-27 17:44:56 -0500

    Add improvements to code formatting.

--- benchmark.py
+++ benchmark.py
@@ -7,104 +7,20 @@ from inspect import *
 import sys
 import time
 
-# Set defaults:
+# Set benchmarking defaults:
 input_size_start = 100
 input_growth_factor = 2
 previous_time = 1
 
 
-def get_num_of_rounds():
-    """Recieves user-inputted number of rounds for experiment."""
-    user_rounds = int(input("Please specify a number of rounds: "))
-    return user_rounds
-
-
-def generate_data(current_size, types):
-    """Generates data to be used in experiment."""
-    num_params = len(types)
-    bench_data = []
-
-    for current_type in types:
-        if current_type == "int":
-            # generate int
-            bench_data.append(int(current_size))
-        elif current_type == "float":
-            # generate float
-            default_float = 1.0
-            gen_data = default_float * current_size
-            bench_data.append(gen_data)
-        elif current_type == "list":
-            # generate list
-            default_list = [1]
-            gen_data = default_list * current_size
-            bench_data.append(gen_data)
-        elif current_type == "string":
-            # generate string
-            default_string = "str"
-            gen_data = default_string * current_size
-            bench_data.append(gen_data)
-        elif current_type == "char":
-            # generate char
-            default_char = "c"
-            gen_data = default_char * current_size
-            bench_data.append(gen_data)
-        else:
-            print("Unsupported data type:", current_type)
-
-    params = bench_data
-    print("TEST_PARAMS", params)
-    return params
-
-
-def run_benchmark(previous_time, user_module, function, types, run_function, user_rounds):
-    current_size = input_size_start
-
-    round_num = 1 # set the starting round number
-
-    while(round_num <= user_rounds):
-        if round_num == 1:
-            current_size = 100 # set the starting size
-        else:
-            current_size = current_size * input_growth_factor # update size for post-round 1 rounds
-
-        params = generate_data(current_size, types) # generate data and store as parameters
-
-        # Start the benchmark for current round
-        start_time = time.time() # start timer
-
-        run_function(*params) # run the function with parameter data size
-
-        stop_time = time.time() # stop timer
-        time_elapsed = stop_time - start_time # calculate function run time
-
-        if(round_num == 1):
-            avg_runtime = 0 # no previous rounds so avg runtime is 0
-        else:
-            avg_runtime = time_elapsed / previous_time
-
-        print("Round", round_num, " --- Size:", current_size, " --- ", time_elapsed, " --- AVG RUN: ", avg_runtime)
-
-        previous_time = time_elapsed
-        round_num += 1
-
-
-def start_up_message():
-    """Creates and displays a simple welcome message."""
-    print("****************************************************************")
-    print("*          Speed-Surprises Simple Benchmarking Tool            *")
-    print("****************************************************************")
-    print() # print balnk line for spacing
-
-
 def main(argv):
     """Driver function that gets arguments and runs necessary functions."""
-
-    # Create variables for storing arguments
+    # Create variables for storing arguments:
     module = ""
     function = ""
     types = []
 
-    # Get arguments
+    # Get arguments:
     try:
         opts, args = getopt.getopt(argv, "m:f:t:h", ["module=", "function=", "types=",  "help"])
     except getopt.GetoptError:
@@ -140,12 +56,6 @@ def main(argv):
         else:
             print("benchmark.py -m <module> -f <function> -t <types>")
 
-    # print the user's inputted arguments for testing purposes
-    print("***User Arguments***")
-    print("Module:", module)
-    print("Function:", function)
-    print("types:", types)
-
     user_module = importlib.import_module(module) # import argument's module
     run_function = getattr(user_module, function) # code that will run function when called
 
@@ -158,6 +68,85 @@ def main(argv):
 
     # run the benchmark:
     run_benchmark(previous_time, user_module, function, types, run_function, user_rounds)
+
+
+def generate_data(current_size, types):
+    """Generates data to be used in experiment."""
+    bench_data = [] # create list to store generated data
+
+    for current_type in types:
+        if current_type == "int":
+            # generate int
+            bench_data.append(int(current_size))
+        elif current_type == "float":
+            # generate float
+            default_float = 1.0
+            gen_data = default_float * current_size
+            bench_data.append(gen_data)
+        elif current_type == "list":
+            # generate list
+            default_list = [1]
+            gen_data = default_list * current_size
+            bench_data.append(gen_data)
+        elif current_type == "string":
+            # generate string
+            default_string = "str"
+            gen_data = default_string * current_size
+            bench_data.append(gen_data)
+        elif current_type == "char":
+            # generate char
+            default_char = "c"
+            gen_data = default_char * current_size
+            bench_data.append(gen_data)
+        else:
+            print("Unsupported data type:", current_type)
+    params = bench_data # store list of generated data in params
+
+    return params
+
+
+def run_benchmark(previous_time, user_module, function, types, run_function, user_rounds):
+    current_size = input_size_start
+    round_num = 1 # set the starting round number
+
+    while(round_num <= user_rounds):
+        if round_num == 1:
+            current_size = 100 # set the starting size
+        else:
+            current_size = current_size * input_growth_factor # update size for post-round 1 rounds
+
+        params = generate_data(current_size, types) # generate data and store as parameters
+
+        # Start the benchmark for current round:
+        start_time = time.time() # start timer
+        run_function(*params) # run the function with parameter data size
+        stop_time = time.time() # stop timer
+        time_elapsed = stop_time - start_time # calculate function run time
+
+        if(round_num == 1):
+            avg_runtime = 0 # no previous rounds; avg runtime is 0
+        else:
+            avg_runtime = time_elapsed / previous_time
+
+        print("Round", round_num, " --- Size:", current_size, " --- ", time_elapsed, " --- AVG RUN: ", avg_runtime)
+
+        previous_time = time_elapsed
+        round_num += 1
+
+
+def get_num_of_rounds():
+    """Recieves user-inputted number of rounds for experiment."""
+    user_rounds = int(input("Please specify a number of rounds: "))
+    return user_rounds
+
+
+def start_up_message():
+    """Creates and displays a simple welcome message."""
+    print("****************************************************************")
+    print("*          Speed-Surprises Simple Benchmarking Tool            *")
+    print("****************************************************************")
+    print() # print balnk line for spacing
+
 
 # run main - run program
 if __name__ == "__main__":
