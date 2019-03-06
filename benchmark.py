221e4bb25e0a00c954e5b22bcb9e781360d4d852
clussier <lussierc@allegheny.edu>
2019-03-06 16:41:25 -0500

    Reformat benchmark code with black.

--- benchmark.py
+++ benchmark.py
@@ -14,7 +14,9 @@ input_size_start = 100
 input_growth_factor = 2
 previous_time = 1
 
-results_table = PrettyTable(['Round', 'Size', 'Runtime', 'Average']) # create results table
+results_table = PrettyTable(
+    ["Round", "Size", "Runtime", "Average"]
+)  # create results table
 
 
 def main(argv):
@@ -26,57 +28,69 @@ def main(argv):
 
     # Get arguments:
     try:
-        opts, args = getopt.getopt(argv, "m:f:t:h", ["module=", "function=", "types=",  "help"])
+        opts, args = getopt.getopt(
+            argv, "m:f:t:h", ["module=", "function=", "types=", "help"]
+        )
     except getopt.GetoptError:
         print("Incorrect Format!")
         print("benchmark.py -m <module> -f <function> -t <types>")
-        print() # print blank line for spacing
+        print()  # print blank line for spacing
         print("Type benchmark.py -h for more help.")
-        print() # print blank line for spacing
+        print()  # print blank line for spacing
         sys.exit(2)
     for opt, arg in opts:
-        if opt == '-h':
+        if opt == "-h":
             # program help information
             print("****************************************************************")
             print("*                      S-S Benchmark Help:                     *")
             print("****************************************************************")
-            print() # print blank line for spacing
+            print()  # print blank line for spacing
             print("Format:  benchmark.py -m <module> -f <function> -t <types>")
-            print() # print blank line for spacing
+            print()  # print blank line for spacing
             print("Sample Usage:")
-            print("python3 benchmark.py -m speedsurprises.numbers.factorial -f compute_factorial -t int")
-            print("python3 benchmark.py -m speedsurprises.lists.sets -f is_subset -t list,list")
-            print() # print blank line for spacing
+            print(
+                "python3 benchmark.py -m speedsurprises.numbers.factorial -f compute_factorial -t int"
+            )
+            print(
+                "python3 benchmark.py -m speedsurprises.lists.sets -f is_subset -t list,list"
+            )
+            print()  # print blank line for spacing
             print("Currently supported types: int, float, list, string, char")
-            print() # print blank line for spacing
-            print("* When entering types, for each parameter in your chosen function put it's type. \nFor example, if a functions paramters are two lists, enter 'list,list' for the types.")
+            print()  # print blank line for spacing
+            print(
+                "* When entering types, for each parameter in your chosen function put it's type. \nFor example, if a functions paramters are two lists, enter 'list,list' for the types."
+            )
             sys.exit(2)
         elif opt in ("-m", "--module"):
             module = arg
         elif opt in ("-f", "--function"):
             function = arg
         elif opt in ("-t", "--types"):
-            types = arg.replace(' ','').split(',')
+            types = arg.replace(" ", "").split(",")
         else:
             print("benchmark.py -m <module> -f <function> -t <types>")
 
-    user_module = importlib.import_module(module) # import argument's module
-    run_function = getattr(user_module, function) # code that will run function when called
+    user_module = importlib.import_module(module)  # import argument's module
+    run_function = getattr(
+        user_module, function
+    )  # code that will run function when called
 
-    start_up_message() # display start up message
+    start_up_message()  # display start up message
 
-    user_rounds = get_num_of_rounds() # get number of user-chosen rounds
+    user_rounds = get_num_of_rounds()  # get number of user-chosen rounds
 
-    print() # print blank line for spacing
+    print()  # print blank line for spacing
     print("Running a doubling-experiment benchmark for", function, "...")
 
     # run the benchmark:
-    run_benchmark(previous_time, user_module, function, types, run_function, user_rounds)
+    run_benchmark(
+        previous_time, user_module, function, types, run_function, user_rounds
+    )
 
 
 def generate_data(current_size, types):
     """Generates data to be used in experiment."""
-    bench_data = [] # create list to store generated data
+    bench_data = []  # create list to store generated data
 
     for current_type in types:
         if current_type == "int":
@@ -104,49 +118,56 @@ def generate_data(current_size, types):
             bench_data.append(gen_data)
         else:
             print("Unsupported data type:", current_type)
-    params = bench_data # store list of generated data in params
+    params = bench_data  # store list of generated data in params
 
     return params
 
 
-def run_benchmark(previous_time, user_module, function, types, run_function, user_rounds):
+def run_benchmark(
+    previous_time, user_module, function, types, run_function, user_rounds
+):
     current_size = input_size_start
-    round_num = 1 # set the starting round number
+    round_num = 1  # set the starting round number
 
-    while(round_num <= user_rounds):
+    while round_num <= user_rounds:
         if round_num == 1:
-            current_size = 100 # set the starting size
+            current_size = 100  # set the starting size
         else:
-            current_size = current_size * input_growth_factor # update size for post-round 1 rounds
+            current_size = (
+                current_size * input_growth_factor
+            )  # update size for post-round 1 rounds
 
-        print() # print blank line for spacing
+        print()  # print blank line for spacing
         print("Running round", round_num, "with size of", current_size, "...")
 
-        params = generate_data(current_size, types) # generate data and store as parameters
+        params = generate_data(
+            current_size, types
+        )  # generate data and store as parameters
 
         # Start the benchmark for current round:
-        start_time = time.time() # start timer
-        run_function(*params) # run the function with parameter data size
-        stop_time = time.time() # stop timer
-        time_elapsed = stop_time - start_time # calculate function run time
+        start_time = time.time()  # start timer
+        run_function(*params)  # run the function with parameter data size
+        stop_time = time.time()  # stop timer
+        time_elapsed = stop_time - start_time  # calculate function run time
 
-        if(round_num == 1):
-            avg_runtime = 0 # no previous rounds; avg runtime is 0
+        if round_num == 1:
+            avg_runtime = 0  # no previous rounds; avg runtime is 0
         else:
             avg_runtime = time_elapsed / previous_time
 
         add_results(results_table, round_num, current_size, time_elapsed, avg_runtime)
-        #print("Round", round_num, " --- Size:", current_size, " --- ", time_elapsed, " --- AVG RUN: ", avg_runtime)
+        # print("Round", round_num, " --- Size:", current_size, " --- ", time_elapsed, " --- AVG RUN: ", avg_runtime)
 
         previous_time = time_elapsed
         round_num += 1
-    print() # print blank line for spacing
-    print(results_table) # print the results table
+    print()  # print blank line for spacing
+    print(results_table)  # print the results table
 
 
 def add_results(results_table, round_num, current_size, time_elapsed, avg_runtime):
     """Add elements into the results_table."""
     results_table.add_row([round_num, current_size, time_elapsed, avg_runtime])
+
 
 def get_num_of_rounds():
     """Recieves user-inputted number of rounds for experiment."""
@@ -159,9 +180,9 @@ def start_up_message():
     print("****************************************************************")
     print("*          Speed-Surprises Simple Benchmarking Tool            *")
     print("****************************************************************")
-    print() # print balnk line for spacing
+    print()  # print balnk line for spacing
 
 
 # run main - run program
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
