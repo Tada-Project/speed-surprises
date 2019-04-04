8980ee322e0c602d40f094dccdf9675ccc92256e
Lancaster Wu <wuj@allegheny.edu>
2019-04-04 02:04:18 -0400

    Revert "Revert "Merge pull request #10 from gkapfham/lancasterboolean""
    This reverts commit 3a181f88ececd6b8aa69f1d7f5826633fd74fde9.

---
+++ speedsurprises/boolean/booleancopies.py
@@ -0,0 +1,25 @@
+"""Functions that create copies of boolean and booleanlists"""
+
+Boolean_T = True
+
+
+def bcopies_oft(inputlength):
+    """Create empty list for boolean true"""
+    list_of_boolean_true = []
+    # iterate length of inputlength times, appending a boolean true at each iteration
+    # pylint: disable=unused-variable
+    for i in range(int(inputlength)):
+        list_of_boolean_true.append(Boolean_T)
+    # return copied list
+    return list_of_boolean_true
+
+
+def bcopies_of(inputlength, boolean):
+    """Create empty list for boolean true"""
+    list_of_boolean_true = []
+    # iterate length of inputlength times, appending a boolean true at each iteration
+    # pylint: disable=unused-variable
+    for i in range(int(inputlength)):
+        list_of_boolean_true.append(boolean)
+    # return copied list
+    return list_of_boolean_true
