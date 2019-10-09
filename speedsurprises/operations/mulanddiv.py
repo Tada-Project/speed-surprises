4c6289bcecb4c303dea85aa3ba17209fd00c91da
Lancasterwu <wuj@allegheny.edu>
2019-10-09 16:54:12 -0400

    Added multiply function

---
+++ speedsurprises/operations/mulanddiv.py
@@ -0,0 +1,11 @@
+"""Compute the basic multiplication and division function"""
+
+def compute_addtion(value):
+    """ Assumes the value is a natural number. Returns the value!"""
+    answer = value * value
+    return answer
+
+def compute_subtraction(value):
+    """ Assumes the value is a natural number. Returns the value!"""
+    answer = value / value
+    return answer
