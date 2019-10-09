2930aa755a5c103a95ee35f2d6edc8e26a634f12
Lancasterwu <wuj@allegheny.edu>
2019-10-09 18:03:17 -0400

    Fixed all linting error

--- speedsurprises/operations/mulanddiv.py
+++ speedsurprises/operations/mulanddiv.py
@@ -1,5 +1,6 @@
 """Compute the basic multiplication and division function"""
 
+
 def compute_multiplication(value):
     """ Assumes the value is a natural number. Returns the value!"""
     answer = value * value
