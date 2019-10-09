78f3147bbe0200e6098f6958f493fe799edd3889
Lancasterwu <wuj@allegheny.edu>
2019-10-09 17:58:20 -0400

    Added two empty line

--- speedsurprises/operations/addandsub.py
+++ speedsurprises/operations/addandsub.py
@@ -5,7 +5,8 @@ def compute_addtion(value):
     answer = value + value
     return answer
 
-def compute_subtraction(value):
+
+    def compute_subtraction(value):
     """ Assumes the value is a natural number. Returns the value!"""
     answer = value - value
     return answer
--- speedsurprises/operations/mulanddiv.py
+++ speedsurprises/operations/mulanddiv.py
@@ -5,6 +5,7 @@ def compute_multiplication(value):
     answer = value * value
     return answer
 
+
 def compute_division(value):
     """ Assumes the value is a natural number. Returns the value!"""
     answer = value / value
