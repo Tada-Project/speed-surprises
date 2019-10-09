a934ee3fab829a14c05c35915ca4740e1abf041a
Lancasterwu <wuj@allegheny.edu>
2019-10-09 16:54:27 -0400

    Added Addition and Subtraction function

---
+++ speedsurprises/operations/addandsub.py
@@ -0,0 +1,11 @@
+"""Compute the basic addtion and subtraction function"""
+
+def compute_addtion(value):
+    """ Assumes the value is a natural number. Returns the value!"""
+    answer = value + value
+    return answer
+
+def compute_subtraction(value):
+    """ Assumes the value is a natural number. Returns the value!"""
+    answer = value - value
+    return answer
