9c962f472dd1f5e297d716bb63274c8ded92e803
Lancasterwu <wuj@allegheny.edu>
2019-10-10 11:54:37 -0400

    Added a simple linear function

--- speedsurprises/search/basic_search.py
+++ speedsurprises/search/basic_search.py
@@ -1,2 +1,14 @@
+"""Compute basic search functions"""
 # https://www.geeksforgeeks.org/python-program-for-linear-search/
 # https://www.geeksforgeeks.org/python-program-for-binary-search/
+import random
+
+# Worst-case time complexity: O(n)
+
+def compute_linear_search(list):
+    """Search a list using linear search function."""
+    x = random.choice(list)
+    for i in range(len(list)):
+        if list[i] == x:
+            return i
+    return -1
