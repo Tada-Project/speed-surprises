86c1634e2d80209c5bb3b157091546d5b4e96148
Enpu You <youe2@aldenv176.allegheny.edu>
2019-10-10 14:07:45 -0400

    Add binary search

--- speedsurprises/search/basic_search.py
+++ speedsurprises/search/basic_search.py
@@ -14,3 +14,22 @@ def compute_linear_search(list):
         if list[i] == x:
             return i
     return -1
+
+
+def compute_binary_search(list):
+    """Search a list using linear search function."""
+	first = 0
+	last = len(list)-1
+    # Search target set as the last number in the list for the worst case
+    target = list[last]
+	found = False
+	while( first <= last and not found):
+		mid = (first + last)//2
+		if list[mid] == target :
+			found = True
+		else:
+			if item < list[mid]:
+				last = mid - 1
+			else:
+				first = mid + 1
+	return found
