7cd377900d1ce73d82ef8f6da46c2e1df8efa036
enpuyou <youe2@allegheny.edu>
2020-01-04 22:33:40 -0500

    add return statement back to pass test cases

--- speedsurprises/lists/sorting.py
+++ speedsurprises/lists/sorting.py
@@ -14,6 +14,7 @@ def insertion_sort(list):
             list[position] = list[position - 1]
             position -= 1
         list[position] = currentValue
+    return list
 
 
 """Do list sorting using BubbleSort"""
@@ -32,6 +33,7 @@ def bubble_sort(list):
                 temp = list[i]
                 list[i] = list[i + 1]
                 list[i + 1] = temp
+    return list
 
 
 """Do list sorting using MergeSort"""
@@ -73,6 +75,7 @@ def merge_sort(list):
             list[k] = righthalf[j]
             j = j + 1
             k = k + 1
+    return list
 
 
 # This function sorts array from left index to
