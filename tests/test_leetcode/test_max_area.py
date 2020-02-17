3c97cca3949a164e88326161624cb5d605c00855
enpuyou <youe2@allegheny.edu>
2020-02-16 21:50:17 -0500

    Add test case for maxarea

---
+++ tests/test_leetcode/test_max_area.py
@@ -0,0 +1,24 @@
+"""Test for max area problem"""
+
+from speedsurprises.leetcode import max_area
+
+lst = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+expected = 49
+
+
+def test_max_area_log():
+    """Returns output with correct max area"""
+    output = max_area.max_area_log(lst)
+    assert output == expected
+
+
+def test_max_area_linear():
+    """Returns output with correct max area"""
+    output = max_area.max_area_linear(lst)
+    assert output == expected
+
+
+def test_max_area_quadratic():
+    """Returns output with correct max area"""
+    output = max_area.max_area_quadratic(lst)
+    assert output == expected
