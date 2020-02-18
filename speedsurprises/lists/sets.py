5fe14016efe81b0a071d0c6de81ed4b1748cfdb1
Lancaster Wu <wuj@allegheny.edu>
2020-02-18 10:07:42 -0500

    Fixing all linting errors

--- speedsurprises/lists/sets.py
+++ speedsurprises/lists/sets.py
@@ -22,24 +22,24 @@ def is_subset(first_list, second_list):
     return True
 
 
-
 def chinese_remainder(n, a):
     sum = 0
-    prod = reduce(lambda a, b: a*b, n)
+    prod = reduce(lambda a, b: a * b, n)
     for n_i, a_i in zip(n, a):
         p = prod // n_i
         sum += a_i * mul_inv(p, n_i) * p
     return sum % prod
 
 
-
 def mul_inv(a, b):
     b0 = b
     x0, x1 = 0, 1
-    if b == 1: return 1
+    if b == 1:
+        return 1
     while a > 1:
         q = a // b
-        a, b = b, a%b
+        a, b = b, a % b
         x0, x1 = x1 - q * x0, x0
-    if x1 < 0: x1 += b0
+    if x1 < 0:
+        x1 += b0
     return x1
