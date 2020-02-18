964d8fd3d5079db5c2cecae782da8de0c464c53b
Lancaster Wu <wuj@allegheny.edu>
2020-02-18 09:47:31 -0500

    Fixed some linting errors

--- speedsurprises/search/recursive_search.py
+++ speedsurprises/search/recursive_search.py
@@ -2,41 +2,41 @@
 # https://www.geeksforgeeks.org/exponential-search/
 
 
-def compute_recursive_binary_search(arr, l, r, x):
+def compute_recursive_binary_search(list, l, r, x):
     if r >= l:
         mid = int(l + ( r-l ) / 2)
 
         # If the element is present at
         # the middle itself
-        if arr[mid] == x:
+        if list[mid] == x:
             return mid
         # If the element is smaller than mid,
         # then it can only be present in the
         # left subarray
-        if arr[mid] > x:
-            return binarySearch(arr, l,
+        if list[mid] > x:
+            return compute_recursive_binary_search(list, l,
                                 mid - 1, x)
         # Else he element can only be
         # present in the right
-        return binarySearch(arr, mid + 1, r, x)
+        return compute_recursive_binary_search(list, mid + 1, r, x)
     # We reach here if the element is not present
     return -1
 
 
 # Returns the position of first
 # occurrence of x in array
-def compute_exponential_search(arr):
+def compute_exponential_search(list):
     # IF x is present at first
     # location itself
-    n = len(arr)
-    x = arr[len(arr) - 1]
-    if arr[0] == x:
+    n = len(list)
+    x = list[len(list) - 1]
+    if list[0] == x:
         return 0
     # Find range for binary search
     # j by repeated doubling
     i = 1
-    while i < n and arr[i] <= x:
+    while i < n and list[i] <= x:
         i = i * 2
     # Call binary search for the found range
-    return compute_recursive_binary_search(arr, i / 2,
-                         min(i, n), x)
+    return compute_recursive_binary_search(list, i / 2,
+                                           min(i, n), x)
