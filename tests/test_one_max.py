9c5dadc487a556399b36d0c8bc0322ae3307c8a2
enpuyou <youe2@allegheny.edu>
2020-03-09 22:48:09 -0400

    Change to parametrize test

--- tests/test_one_max.py
+++ tests/test_one_max.py
@@ -1,9 +1,13 @@
 """Test for the one max problem with GA"""
 
+import pytest
 from speedsurprises.genetic_algorithm import onemax
 
 
-def test_onemax_size_100():
+@pytest.mark.parametrize(
+    "input_amount, expected_answer", [(50, 50), (100, 100), (200, 200)],
+)
+def test_onemax_size_100(input_amount, expected_answer):
     """Test if onemax return all ones"""
     output = sum(onemax.onemax(100))
     expect = 100
