fe05c50e5507bae5f967ef5aafc8a4235ad7019c
enpuyou <youe2@allegheny.edu>
2021-06-04 19:37:59 -0400

    Add a custom data generation function for dictionary

---
+++ speedsurprises/data_generation/custom_data_generation.py
@@ -0,0 +1,8 @@
+"""Sample data generation functions."""
+import random
+
+
+def generate_dict_and_int(chosen_size):
+    """Generate a dictionary and an int"""
+    output = {k: random.random() for k in range(int(chosen_size))}
+    return (output, random.random())
