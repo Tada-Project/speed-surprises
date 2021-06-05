6c5f1aa540a639baa0bb547a0aa6757475a9570f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-06-05 19:04:31 -0400

    Improve the docstrings and comments and format in graph_gen.

--- speedsurprises/graph/graph_gen.py
+++ speedsurprises/graph/graph_gen.py
@@ -1,11 +1,18 @@
+"""Perform depth-first search and breadth-first search on graphs."""
+
+# Reference:
 # https://github.com/TheAlgorithms/Python/blob/master/graphs/graph_list.py
 
 
 class AdjacencyList:
+    """Adjacency list representation of a graph."""
+
     def __init__(self):
+        """Construct a graph with an empty adjacency list."""
         self.List = {}
 
     def addEdge(self, fromVertex, toVertex):
+        """Add an edge to the graph by updating adjacency list."""
         # check if vertex is already present
         if fromVertex in self.List.keys():
             self.List[fromVertex].append(toVertex)
@@ -13,11 +20,15 @@ class AdjacencyList:
             self.List[fromVertex] = [toVertex]
 
     def printList(self):
+        """Add an edge to the graph by updating adjacency list."""
         for i in self.List:
+            # do not produce output and instead perform it as computation
             # print((i, "->", " -> ".join([str(j) for j in self.List[i]])))
+            # pylint: disable=unused-variable
             a = (i, "->", " -> ".join([str(j) for j in self.List[i]]))  # noqa: F841
 
     def BFS(self, s):
+        """Perform a breadth-first search of the graph."""
         visited = [False] * (len(self.List))
         queue = []
         queue.append(s)
@@ -30,17 +41,20 @@ class AdjacencyList:
                     visited[i] = True
 
     def DFSUtil(self, v, visited):
+        """Check to see if a node is visited."""
         visited[v] = True
         for i in self.List[v]:
             if visited[i] is False:
                 self.DFSUtil(i, visited)
 
     def DFS(self, v):
+        """Perform a depth-first search of the graph."""
         visited = [False] * (max(self.List) + 1)
         self.DFSUtil(v, visited)
 
 
 # pipenv run python tada_a_bigoh.py --directory ../speed_surprises/ --module=speedsurprises.graph.graph_gen --function=graph_gen --types hypothesis --schema=../speed_surprises/speedsurprises/jsonschema/int_and_int.json --startsize=50 --max=1000
+
 # Quit due to researched max size
 # +------+-----------------------+-----------------------+--------------------+
 # | Size |          Mean         |         Median        |       Ratio        |
@@ -55,6 +69,7 @@ class AdjacencyList:
 
 
 def graph_gen(n_node, n_edge):
+    """Generate a graph in the form of an adjacency list."""
     g = AdjacencyList()
     for no in range(n_node):
         for ed in range(n_edge):
@@ -62,6 +77,7 @@ def graph_gen(n_node, n_edge):
 
 
 # pipenv run python tada_a_bigoh.py --directory ../speed_surprises/ --module=speedsurprises.graph.graph_gen --function=print_graph_gen --types hypothesis --schema=../speed_surprises/speedsurprises/jsonschema/int_and_int.json --startsize=50 --max=1000
+
 # Quit due to researched max size
 # +------+-----------------------+-----------------------+--------------------+
 # | Size |          Mean         |         Median        |       Ratio        |
@@ -76,14 +92,18 @@ def graph_gen(n_node, n_edge):
 
 
 def print_graph_gen(n_node, n_edge):
+    """Display a graph after generating it in the form of an adjacency list."""
     g = AdjacencyList()
     for no in range(n_node):
         for ed in range(n_edge):
             g.addEdge(no, ed)
     g.printList()
 
+# Reference:
+# https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
 
 # pipenv run python tada_a_bigoh.py --directory ../speed_surprises/ --module=speedsurprises.graph.graph_gen --function=graph_gen_BFS --types hypothesis --schema=../speed_surprises/speedsurprises/jsonschema/int_and_int.json --startsize=50  --max=1000
+
 # Quit due to researched max size
 # +------+-----------------------+---------------------+--------------------+
 # | Size |          Mean         |        Median       |       Ratio        |
@@ -95,16 +115,19 @@ def print_graph_gen(n_node, n_edge):
 # | 800  |      0.252504645      | 0.23859980000000003 | 4.1427564606875915 |
 # +------+-----------------------+---------------------+--------------------+
 # O(n^2) quadratic
-# https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
 
 
 def graph_gen_BFS(n_node, n_edge):
+    """Generate a graph in an adjacency list and then perform a breadth-first search."""
     g = AdjacencyList()
     for no in range(n_node):
         for ed in range(n_edge):
             g.addEdge(no, ed)
     g.BFS(0)
 
+
+# Reference:
+# https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/?ref=rp
 
 # pipenv run python tada_a_bigoh.py --directory ../speed_surprises/ --module=speedsurprises.graph.graph_gen --function=graph_gen_DFS --types hypothesis --schema=../speed_surprises/speedsurprises/jsonschema/int_and_int.json --startsize=50  --max=1000
 # Quit due to researched max size
@@ -118,10 +141,10 @@ def graph_gen_BFS(n_node, n_edge):
 # | 800  |   0.2611786583333333  |  0.25149170000000004  | 4.153683886036841  |
 # +------+-----------------------+-----------------------+--------------------+
 # O(n^2) quadratic
-# https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/?ref=rp
 
 
 def graph_gen_DFS(n_node, n_edge):
+    """Generate a graph in an adjacency list and then perform a depth-first search."""
     g = AdjacencyList()
     for no in range(n_node):
         for ed in range(n_edge):
