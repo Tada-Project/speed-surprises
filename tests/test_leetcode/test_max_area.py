62f3f5f81b2f5be27cdb2203243ba764db288d6d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-06-05 19:05:47 -0400

    Improve the docstrings in the test_max_area.

--- tests/test_leetcode/test_max_area.py
+++ tests/test_leetcode/test_max_area.py
@@ -1,4 +1,4 @@
-"""Test for max area problem"""
+"""Test for max area problem."""
 
 from speedsurprises.leetcode import max_area
 
@@ -7,18 +7,18 @@ expected = 49
 
 
 def test_max_area_log():
-    """Returns output with correct max area"""
+    """Returns output with correct max area."""
     output = max_area.max_area_log(lst)
     assert output == expected
 
 
 def test_max_area_linear():
-    """Returns output with correct max area"""
+    """Returns output with correct max area."""
     output = max_area.max_area_linear(lst)
     assert output == expected
 
 
 def test_max_area_quadratic():
-    """Returns output with correct max area"""
+    """Returns output with correct max area."""
     output = max_area.max_area_quadratic(lst)
     assert output == expected
