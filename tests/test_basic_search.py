2eabdca7f07e970f5ee59aa194b2d7a47ea60374
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-06-05 17:58:13 -0400

    Reformat the test_basic_search and improve comments.

--- tests/test_basic_search.py
+++ tests/test_basic_search.py
@@ -1,4 +1,4 @@
-"""Tests for the basic searching function in the basic_search module of the search package"""
+"""Tests for the basic searching function in the basic_search module of the search package."""
 
 import pytest
 
@@ -7,71 +7,73 @@ from speedsurprises.search import basic_search
 
 @pytest.mark.benchmark
 def test_linear_search_benchmark_found(benchmark):
-    """Benchmark the insertionSort function"""
+    """Benchmark the insertionSort function."""
     position = benchmark(
-        basic_search.compute_linear_search, list=[1, 2, 3, 4], x=2
+        basic_search.compute_linear_search, search_list=[1, 2, 3, 4], x=2
     )
     assert position == 1
 
 
 @pytest.mark.benchmark
 def test_linear_search_benchmark_notfound(benchmark):
-    """Benchmark the insertionSort function"""
+    """Benchmark the insertionSort function."""
     notfound = benchmark(
-        basic_search.compute_linear_search, list=[1, 2, 3, 4], x=10
+        basic_search.compute_linear_search, search_list=[1, 2, 3, 4], x=10
     )
     assert notfound == -1
 
 
 @pytest.mark.benchmark
 def test_iterative_binary_search_benchmark_found(benchmark):
-    """Benchmark the iterative_binary_search function"""
+    """Benchmark the iterative_binary_search function."""
     position = benchmark(
-        basic_search.compute_iterative_binary_search, list=[1, 2, 3, 4], target=2
+        basic_search.compute_iterative_binary_search, search_list=[1, 2, 3, 4], target=2
     )
     assert position == 1
 
 
 @pytest.mark.benchmark
 def test_iterative_binary_search_benchmark_notfound(benchmark):
-    """Benchmark the iterative_binary_search function"""
+    """Benchmark the iterative_binary_search function."""
     notfound = benchmark(
-        basic_search.compute_iterative_binary_search, list=[1, 2, 3, 4], target=10
+        basic_search.compute_iterative_binary_search,
+        search_list=[1, 2, 3, 4],
+        target=10,
     )
     assert notfound == -1
 
 
 @pytest.mark.benchmark
 def test_jump_search_benchmark_found(benchmark):
-    """Benchmark the jump_search function"""
+    """Benchmark the jump_search function."""
     position = benchmark(
-        basic_search.compute_jump_search, list=[1, 2, 3, 4], x=2
+        basic_search.compute_jump_search, search_list=[1, 2, 3, 4], x=2
     )
     assert position == 1
 
 
 @pytest.mark.benchmark
 def test_jump_search_benchmark_notfound(benchmark):
-    """Benchmark the jump_search function"""
+    """Benchmark the jump_search function."""
     notfound = benchmark(
-        basic_search.compute_jump_search, list=[1, 2, 3, 4], x=10
+        basic_search.compute_jump_search, search_list=[1, 2, 3, 4], x=10
     )
     assert notfound == -1
 
 
 @pytest.mark.benchmark
 def test_compute_interpolation_search_benchmark_found(benchmark):
-    """Benchmark the interpolation_search function"""
+    """Benchmark the interpolation_search function."""
     position = benchmark(
-        basic_search.compute_interpolation_search, list=[1, 2, 3, 4], x=2
+        basic_search.compute_interpolation_search, search_list=[1, 2, 3, 4], x=2
     )
     assert position == 1
 
 
 @pytest.mark.benchmark
 def test_compute_interpolation_search_benchmark_notfound(benchmark):
-    """Benchmark the interpolation_search function"""
+    """Benchmark the interpolation_search function."""
     notfound = benchmark(
-        basic_search.compute_interpolation_search, list=[1, 2, 3, 4], x=10
+        basic_search.compute_interpolation_search, search_list=[1, 2, 3, 4], x=10
     )
     assert notfound == -1
