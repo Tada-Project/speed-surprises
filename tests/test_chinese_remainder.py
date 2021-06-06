bf08893027ae3cafd5553e6817f1456e85c331a9
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-06-05 20:39:32 -0400

    Fix docstring comments in the test_chinese_remainder.

--- tests/test_chinese_remainder.py
+++ tests/test_chinese_remainder.py
@@ -1,4 +1,4 @@
-"""Tests for the chinese_remainder function in the sets module of the lists package"""
+"""Tests for the chinese_remainder function in the sets module of the lists package."""
 
 import pytest
 
@@ -7,8 +7,6 @@ from speedsurprises.lists import sets
 
 @pytest.mark.benchmark
 def test_chinese_remainder_benchmark(benchmark):
-    """Benchmark the chinese_remainder function"""
-    remainder = benchmark(
-        sets.chinese_remainder, n=[3, 5, 7], a=[2, 3, 2]
-    )
+    """Benchmark the chinese_remainder function."""
+    remainder = benchmark(sets.chinese_remainder, n=[3, 5, 7], a=[2, 3, 2])
     assert remainder == 23.0
