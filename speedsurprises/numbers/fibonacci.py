3a5abcf7b3de4314246209113e609a4335f6f993
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-06-05 21:44:27 -0400

    Fix docstrings and formatting and code structure for fibonacci.

--- speedsurprises/numbers/fibonacci.py
+++ speedsurprises/numbers/fibonacci.py
@@ -1,4 +1,4 @@
-""" Compute the fibonacci function."""
+"""Compute the output of the fibonacci function."""
 
 # Inspiration and source for the function:
 # Previous works.
@@ -6,23 +6,21 @@
 
 
 def compute_recursive_fibonacci(value):
-    """ Assumes the value is a natural number. Returns the value!"""
+    """Compute fibonacci number in recursive fashion, assumes the value is a natural number."""
     if value <= 1:
         return value
-    else:
-        return compute_recursive_fibonacci(value - 2) + compute_recursive_fibonacci(
-            value - 1
-        )
+    return compute_recursive_fibonacci(value - 2) + compute_recursive_fibonacci(value - 1)
 
 
 def compute_iterative_fibonacci(value):
+    """Compute the fibonacci number in an iterative fashion."""
     if value <= 1:
         return value
-    else:
-        x = 0
-        y = 1
-        for i in range(1, value):
-            z = x + y
-            x = y
-            y = z
-        return y
+    x = 0
+    y = 1
+    # pylint: disable=unused-variable
+    for i in range(1, value):
+        z = x + y
+        x = y
+        y = z
+    return y
